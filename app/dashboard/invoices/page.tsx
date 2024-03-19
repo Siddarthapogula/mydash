d1e53934657522825ba360df6981a9628b0d056d
Siddartha pogula <pogulaSiddharth96@gmail.com>
2024-03-19 23:33:09 +0530

    initial commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Invoices (){
+    return (
+        <h1>This is invoices page</h1>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import NavLinks from "../ui/dashboard/nav-links"
+export default function Dashboard(){
+    return(
+        <NavLinks/>
+        // <h1>hey there</h1>
+    )
+}
